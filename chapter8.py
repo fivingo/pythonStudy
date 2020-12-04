bc3fffa0aaa9a274a8ab984c1a6d47caefb2c48e
fivingo <fiving@naver.com>
2020-12-04 16:26:09 +0900

    20.12.04

--- chapter8.py
+++ chapter8.py
@@ -3,3 +3,25 @@
 # 8.1 딕셔너리
 
 # 8.1.1 생성하기: {}
+empty_dict = {}
+print(empty_dict)
+
+bierce = {
+    "day": "A perid of twenty-four hours, mostly miispent",
+    "positive": "Mistaken at the top of one's voice",
+    "misfortune": "The kind of fortune that never misses",
+}
+print(bierce)
+
+print()
+
+# 8.1.2 생성하기: dict()
+acme_customer = {'first': 'Wile', 'middle': 'E', 'last': 'Coyote'}
+print(acme_customer)
+acme_customer = dict(first="Wile", middle="E", last="Coyote")
+print(acme_customer)
+print()
+
+# 8.1.3 변환하기: dict()
+lol = [['a', 'b'], ['c', 'd'], ['e', 'f']]
+print(dict(lol))
