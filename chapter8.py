bdafdc39c9e31ec8f10cc3512d9591407c82b773
fivingo <fiving@naver.com>
2020-12-06 20:14:25 +0900

    20.12.06

--- chapter8.py
+++ chapter8.py
@@ -53,6 +53,58 @@ print(pythons)
 pythons['Gilliam'] = 'Terry'
 print(pythons)
 
+some_pythons = {
+    'Graham': 'Chapman',
+    'John': 'Cleese',
+    'Eric': 'Idle',
+    'Terry': 'Gilliam',
+    'Michael': 'Palin',
+    'Terry': 'Jones',
+}
+print(some_pythons)
+
+print()
+
+# 8.1.5 항목 얻기: [key]와 get()
+print(some_pythons['John'])
+print('Groucho' in some_pythons)
+print(some_pythons.get('John'))
+print(some_pythons.get('Groucho', 'Not a Python'))
+print(some_pythons.get('Groucho'))
+
+print()
+
+# 8.1.6 모든 키 얻기: keys()
+signals = {'green': 'go', 'yellow': 'go faster', 'red': 'smile for the camera'}
+print(signals.keys())
+print(list(signals.keys()))
+
+print()
+
+# 8.1.7 모든 값 얻기: values()
+print(list(signals.values()))
+
+print()
+
+# 8.1.8 모든 키-값 얻기: items()
+print(list(signals.items()))
+
+print()
+
+# 8.1.9 길이 얻기: len()
+print(len(signals))
+
+print()
+
+# 8.1.10 결합하기: (**a, **b)
+first = {'a': 'agony', 'b': 'bliss'}
+second = {'b': 'bagels', 'c': 'candy'}
+print({**first, **second})
+
+third = {'d': 'donuts'}
+print({**first, **third, **second})
+
+
 
 
 
