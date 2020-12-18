1304c9eaab60681f6d9eb4b77ccb45bd4bdb5b47
fivingo <fiving@naver.com>
2020-12-18 16:53:39 +0900

    20.12.18

--- part I/chapter11/chapter11.py
+++ part I/chapter11/chapter11.py
@@ -182,5 +182,33 @@ pprint(quotes)
 print()
 
 # 11.3.7 임의값 얻기
+from random import choice
+print(choice([23, 9, 46, 'bacon', 0x123abc]))
+print(choice(('a', 'one', 'and-a', 'two')))
+print(choice(range(100)))
+print(choice('alphabet'))
+
+from random import sample
+print(sample([23, 9, 46, 'bacon', 0x123abc], 3))
+print(sample(('a', 'one', 'and-a', 'two'), 2))
+print(sample(range(100), 4))
+print(sample('alphabet', 7))
+
+from random import randint
+print(randint(38, 74))
+print(randint(38, 74))
+print(randint(38, 74))
+
+from random import randrange
+print(randrange(38, 74))
+print(randrange(38, 74, 10))
+print(randrange(38, 74, 10))
+
+from random import random
+print(random())
+print(random())
+print(random())
+
+
 
 
