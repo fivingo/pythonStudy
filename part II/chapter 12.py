13f26a48754b62b90fc7f959ac67ff68177867ea
fivingo <fiving@naver.com>
2020-12-22 15:40:38 +0900

    20.12.22

--- part II/chapter 12.py
+++ part II/chapter 12.py
@@ -289,14 +289,68 @@ print()
 
 # 12.3 이진 데이터
 
+# 12.3.1 바이트와 바이트 배열
+blist = [1, 2, 3, 255]
+the_bytes = bytes(blist)
+print(the_bytes)
+the_byte_array = bytearray(blist)
+print(the_byte_array)
 
+print(b'\x61')
+print(b'\x01abc\xff')
 
+blist = [1, 2, 3, 255]
+the_bytes = bytes(blist)
+# the_bytes[1] = 127 # TypeError: 'bytes' object does not support item assignment
 
+blist = [1, 2, 3, 255]
+the_byte_array = bytearray(blist)
+print(the_byte_array)
+the_byte_array[1] = 127
+print(the_byte_array)
 
+the_bytes = bytes(range(0, 256))
+the_byte_array = bytearray(range(0, 256))
 
+print(the_bytes)
 
+print()
 
+# 12.3.2 이진 데이터 변환하기: struct
+import struct
+valid_png_header = b'\x89PNG\r\n\x1a\n'
+data = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR' + \
+    b'\x00\x00\x00\x9a\x00\x00\x00\x8d\x08\x02\x00\x00\x00\xc0'
+if data[:8] == valid_png_header:
+    width, height = struct.unpack('>LL', data[16:24])
+    print('Valid PNG, width', width, 'height', height)
+else:
+    print('Not a valid PNG')
 
+print(data[16:20])
+print(data[20:24])
 
+print(0x9a)
+print(0x8d)
 
+print(struct.pack('>L', 154))
+print(struct.pack('>L', 141))
 
+print(struct.unpack('>2L', data[16:24]))
+
+print(struct.unpack('>16x2L6x', data))
+
+print()
+
+# 12.3.3 기타 이진 데이터 도구
+
+# 12.3.4 바이트/문자열 변환하기: binacsii()
+import binascii
+valid_png_header = b'\x89PNG\r\n\x1a\n'
+print(binascii.hexlify(valid_png_header))
+
+print(binascii.unhexlify(b'98504e470d0a1a0a'))
+
+print()
+
+# 12.3.5 비트 연산자
