dea964c286db29eaa45977d74391390b205d5fc5
fivingo <fiving@naver.com>
2020-12-29 17:09:38 +0900

    20.12.29

--- part II/chapter14/chapter14.py
+++ part II/chapter14/chapter14.py
@@ -21,19 +21,19 @@ In a relative way,
 And returned on the previous night.'''
 print(len(poem))
 
-fout = open('relativity', 'wt')
+fout = open('relativity.txt', 'wt')
 print(fout.write(poem))
 fout.close()
 
-fout = open('relativity', 'wt')
+fout = open('relativity.txt', 'wt')
 print(poem, file=fout)
 fout.close()
 
-fout = open('relativity', 'wt')
+fout = open('relativity.txt', 'wt')
 print(poem, file=fout, sep='', end='')
 fout.close()
 
-fout = open('relativity', 'wt')
+fout = open('relativity.txt', 'wt')
 size = len(poem)
 offset = 0
 chunk = 100
@@ -44,23 +44,23 @@ while True:
     offset += chunk
 fout.close()
 
-# fout = open('relativity', 'xt') # FileExistsError: [Errno 17] File exists: 'relativity'
+# fout = open('relativity.txt', 'xt') # FileExistsError: [Errno 17] File exists: 'relativity.txt'
 try:
-    fout = open('relativity', 'xt')
+    fout = open('relativity.txt', 'xt')
     fout.write('stomp stomp stomp')
 except FileExistsError:
-    print('relativity already exists! That was a close one.')
+    print('relativity.txt already exists! That was a close one.')
 
 print()
 
 # 14.1.4 텍스트 파일 읽기: read(), readline(), readlines()
-fin = open('relativity', 'rt')
+fin = open('relativity.txt', 'rt')
 poem = fin.read()
 fin.close()
 print(len(poem))
 
 poem = ''
-fin = open('relativity', 'rt')
+fin = open('relativity.txt', 'rt')
 chunk = 100
 while True:
     fragment = fin.read(chunk)
@@ -71,7 +71,7 @@ fin.close()
 print(len(poem))
 
 poem = ''
-fin = open('relativity', 'rt')
+fin = open('relativity.txt', 'rt')
 while True:
     line = fin.readline()
     if not line:
@@ -81,13 +81,13 @@ fin.close()
 print(len(poem))
 
 poem = ''
-fin = open('relativity', 'rt')
+fin = open('relativity.txt', 'rt')
 for line in fin:
     poem += line
 fin.close()
 print(len(poem))
 
-fin = open('relativity', 'rt')
+fin = open('relativity.txt', 'rt')
 lines = fin.readline()
 fin.close()
 print(len(lines), 'lines read')
@@ -100,11 +100,11 @@ print()
 bdata = bytes(range(0, 256))
 print(len(bdata))
 
-fout = open('bfile', 'wb')
+fout = open('bfile.txt', 'wb')
 print(fout.write(bdata))
 fout.close()
 
-fout = open('bfile', 'wb')
+fout = open('bfile.txt', 'wb')
 size = len(bdata)
 offset = 0
 chunk = 100
@@ -117,7 +117,7 @@ while True:
 print()
 
 # 14.1.6 이진 파일 읽: read()
-fin = open('bfile', 'rb')
+fin = open('bfile.txt', 'rb')
 bdata = fin.read()
 print(len(bdata))
 fin.close()
@@ -125,11 +125,11 @@ fin.close()
 print()
 
 # 14.1.7 자동으로 파일 닫기: with
-with open('relativity', 'wt') as fout:
+with open('relativity.txt', 'wt') as fout:
     fout.write(poem)
 
 # 14.1.8 파일 위치 찾기: seek()
-fin = open('bfile', 'rb')
+fin = open('bfile.txt', 'rb')
 print(fin.tell())
 
 print(fin.seek(255))
@@ -143,7 +143,7 @@ print(os.SEEK_SET)
 print(os.SEEK_CUR)
 print(os.SEEK_END)
 
-fin = open('bfile', 'rb')
+fin = open('bfile.txt', 'rb')
 
 print(fin.seek(-1, 2))
 print(fin.tell())
@@ -152,7 +152,7 @@ bdata = fin.read()
 print(len(bdata))
 print(bdata[0])
 
-fin = open('bfile', 'rb')
+fin = open('bfile.txt', 'rb')
 
 print(fin.seek(254, 0))
 print(fin.tell())
